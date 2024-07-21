e9f1460b2a23a69e38180bbb5f5e9d09fb6d4482
warmachine028 <warmachine028@outlook.com>
2024-07-22 00:40:01 +0530

    feat: added dynamic PORT

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,18 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { typeDefs } from './schema.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
+const PORT = parseInt(process.env.PORT)
 const resolvers = {}
-
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 3000 }
+    listen: { port: PORT }
 })
+
 console.log(`🚀 Server ready at: ${url}`)
